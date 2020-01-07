1be34d5655c467f05f5dd70b67cc526a1cc1c04a
Arjen <arjen@bos4.nl>
2020-01-07 19:31:22 +0100

    have the script run and save all leagues when ran as stand alone

--- scrape_538.py
+++ scrape_538.py
@@ -1,9 +1,27 @@
+""" Scrape https://projects.fivethirtyeight.com for soccer predictions
+
+Run this file to scrape all available predictions for various soccer leagues. Results are stored in ./data/538/
+
+"""
+
 from lxml import html
 import requests
 import re
 import pandas as pd
 import unicodedata
-from urls import URLS_538
+
+
+URLS_538 = [
+    "https://projects.fivethirtyeight.com/soccer-predictions" + s
+    for s in [
+        "/eredivisie",
+        "/bundesliga",
+        "/la-liga",
+        "/premier-league",
+        "/ligue-1",
+        "/serie-a",
+    ]
+]
 
 
 def convert_percentage_string_to_float(perc_str):
@@ -13,10 +31,10 @@ def convert_percentage_string_to_float(perc_str):
     return float(m.group()) / 100 if m else None
 
 
-def clean_team_name(team_name):
+def clean_text(text):
     # "Atlético Madrid" to "atleticomadrid"
     return (
-        unicodedata.normalize("NFKD", team_name)
+        unicodedata.normalize("NFKD", text)
         .encode(encoding="ascii", errors="ignore")
         .decode("utf-8")
         .lower()
@@ -25,7 +43,7 @@ def clean_team_name(team_name):
     )
 
 
-def scrape_538(url, verbose=True):
+def scrape_538(url, verbose=True, save_html=False):
     """Scrape one of the 538 pages. Returns a dataframe with the match predictions."""
 
     # Request the page html
@@ -33,8 +51,11 @@ def scrape_538(url, verbose=True):
     tree = html.fromstring(page.content)
 
     # For debugging: Write the html to a file so you can view it in a browser
-    # with open("tmp/page_538.html", "w") as f:
-    #    f.write(str(page.content))
+    if save_html:
+        with open("./data/538/" + re.split(r"/", url)[-1] + ".html", "w") as f:
+            # page.content = bytes and page.text = str
+            f.write(str(page.text))
+            print("Saved as: ./data/538/" + re.split(r"/", url)[-1] + ".html")
 
     # Extract all matches
     matches = tree.cssselect(".games-container.upcoming .match-container")
@@ -57,8 +78,8 @@ def scrape_538(url, verbose=True):
     df["prob_home_win"] = df["prob_home_win"].apply(convert_percentage_string_to_float)
     df["prob_tie"] = df["prob_tie"].apply(convert_percentage_string_to_float)
     df["prob_away_win"] = df["prob_away_win"].apply(convert_percentage_string_to_float)
-    df["home_team"] = df["home_team"].apply(clean_team_name)
-    df["away_team"] = df["away_team"].apply(clean_team_name)
+    df["home_team"] = df["home_team"].apply(clean_text)
+    df["away_team"] = df["away_team"].apply(clean_text)
 
     if verbose:
         print("URL:", url)
@@ -70,8 +91,8 @@ def scrape_538(url, verbose=True):
 
 
 if __name__ == "__main__":
-    # It's only tests here
-
     # Scrape all URLS
     for url in URLS_538:
-        scrape_538(url)
+        df = scrape_538(url, verbose=True, save_html=True)
+        df.to_csv("./data/538/" + re.split(r"/", url)[-1] + ".csv")
+        print("Saved as: ./data/538/" + re.split(r"/", url)[-1] + ".csv")
